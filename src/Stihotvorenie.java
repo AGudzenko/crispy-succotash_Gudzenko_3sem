4acac1c74356199c8a3acb7bc23d6a58c9cedc39
AGudzenko <agydzz@gmail.com>
2020-09-03 18:21:50 +0300

    +Стихотворение

---
+++ src/Stihotvorenie.java
@@ -0,0 +1,16 @@
+public class Stihotvorenie {
+    public String[] str;
+
+    Stihotvorenie (String[] str) {
+        this.str = str;
+    }
+    public void NaitiRifm() {
+        for(int i = 0; i < str.length; i++){
+            for (int j = 0; j < str.length; j++){
+                if(str[i].substring(str[i].length()-3, str[i].length()-1).equals(str[j].substring(str[j].length()-3, str[j].length()-1)) && i != j ){
+                    System.out.print("Строка " + (i+1) + " рифмуется со строкой " + (j+1) + "\n");
+                }
+            }
+        }
+    }
+}
