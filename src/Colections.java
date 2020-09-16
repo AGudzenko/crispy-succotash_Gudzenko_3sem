e4b3412d567a4583c14c622ecfdf3cd4fc3e91c1
AGudzenko <agydzz@gmail.com>
2020-09-16 23:18:14 +0300

    + Коллекции

--- src/Colections.java
+++ src/Colections.java
@@ -10,22 +10,35 @@ public class Colections {
         a.add("ccc30");
         b.add("lalala");
 
-        List<Integer> a_i = new ArrayList<>(zad1());
+        List<String> qq = new ArrayList<>();
+        qq.add("4");
+        qq.add("3");
+        qq.add("6");
+        qq.add("7");
 
+        List<Integer> a_i = new ArrayList<>(zad1());
+        System.out.println("1");
         System.out.println(zad1());
+        System.out.println("2");
         zad2(a, b);
         System.out.println(a);
+        System.out.println("4.2");
         System.out.println(zad42(a));
         System.out.println(a);
+        System.out.println("4.1");
         zad41(a);
         System.out.println(a);
+        System.out.println("5.1.2");
         System.out.println(zad5_1_2(a));
+        System.out.println("5.1.1");
         zad5_1_1(a);
         System.out.println(a);
-
-
+        System.out.println("5.2.2");
+        //System.out.println(zad5_2_2(qq));
+        System.out.println("5.3.2");
         System.out.println(a_i);
         System.out.println(zad5_3_2(a_i));
+        System.out.println("5.3.1");
         zad5_3_1(a_i);
         System.out.println(a_i);
 
@@ -57,7 +70,7 @@ public class Colections {
     }
     public static void zad5_1_1(List<String> a){
         for(int i = 0; i < a.size(); i++){
-            if(i % 2 == 0 && i != 0){
+            if(i % 2 == 0 ){
                 a.remove(i);
             }
         }
@@ -65,12 +78,23 @@ public class Colections {
     public static List<String> zad5_1_2(List<String> a){
         List<String> b = new ArrayList<>(a);
         for(int i = 0; i < a.size(); i++){
-            if(i % 2 == 0 && i != 0){
+            if(i % 2 == 0 ){
                 b.remove(i);
             }
         }
         return b;
     }
+    /*public static List<String> zad5_2_2(List<String> a){
+        List<String> b = new ArrayList<>(a);
+        for(int i = 0; i < a.size(); i++){
+            if( Integer.valueOf(b.get(i)) % 2 == 0 ){
+                b.remove(i);
+            } else {
+
+            }
+        }
+        return b;
+    }*/
     public static void zad5_3_1(List<Integer> a){
         for(int i = 0; i < a.size(); i++){
             if(a.get(i) % 2 == 0 && i != 0){
